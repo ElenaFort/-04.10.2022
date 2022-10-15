cbc35767acd39a30e828f220ab6f7b913c4fb312
Elena <fortygina.lena@mail.ru>
2022-10-15 21:31:56 +0500

    Zadacha 10

---
+++ Zadacha 10/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+void GetNumber(int number)
+{
+    if (number < 1000 && number > 99)
+    {
+        int theFirstTwoDigits = number / 10;
+        int secondDigil = theFirstTwoDigits % 10;
+
+        Console.WriteLine($"Втарая цифра числа {number}: {secondDigil}");
+    }
+    else
+        Console.WriteLine("Введено не трёхзначное число");
+}
+GetNumber(276);
